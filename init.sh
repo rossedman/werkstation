0a6912794950a184049a3d559e60795625e6fc28
Ross Edman <2628905+rossedman@users.noreply.github.com>
2019-06-16 16:56:03 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,6 @@
 set -e
 
 REPO=${REPO:-https://github.com/rossedman/werkstation.git}
-CLONE_DIR=${CLONE_DIR:-/home/"${SUDO_USER}"/src/werkstation}
 
 # install initial packages
 if [ "$(uname -s)" = "Darwin" ]; then
@@ -13,12 +12,5 @@ else
 	apt update
 	apt-get install -y ansible git vim
 fi
-
-# clone this repo
-# mkdir -p "${CLONE_DIR}"
-# git clone --recursive "${REPO}" "${CLONE_DIR}"
-# chown -R ${SUDO_USER}.${SUDO_USER} ${CLONE_DIR}
-
-# ANSIBLE_NOCOWS=1 ansible-playbook -u ${SUDO_USER} -i "${CLONE_DIR}"/hosts "${CLONE_DIR}"/main.yml
 
 ansible-pull -U ${REPO} main.yml
